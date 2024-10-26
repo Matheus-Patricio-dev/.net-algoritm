4737ae31418109e4f5c78dab1fa4b6a22f6885b3
Matheus Patricio <patriciomtt@outlook.com>
2024-10-25 23:07:14 -0300

    adding algoritm files

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triangulo X:");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //dados do triangulo X.
+
+
+            Console.WriteLine("Entre com as medidas do triangulo Y:");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //dados do triangulo Y.
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+            //cria uma variavel P do tipo Double que utiliza a formula de heron.
+
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("VALOR DA AREA X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("VALOR DA AREA Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY) {
+                Console.WriteLine("AREA X é MAIOR");
+            } else {
+                Console.WriteLine("AREA Y é MAIOR");
+            }
+
+
+
+
+
+        }
+    }
+}
